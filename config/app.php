fa0b10366663ee67ca2b6b7f503d998e4817d754
Jai Soni <sonijai64@gmail.com>
2024-04-22 12:48:14 +0530

    check why showing pending on git

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Application Name Laravel Setup
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application. This value is used when the
